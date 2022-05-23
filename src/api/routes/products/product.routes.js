cbe89648dc527ee5af1a16065061a7b1bc895578
Mattis Erkensten <m.erkensten@icloud.com>
2022-05-23 21:31:13 +0200

    Funktionalitet för att hantera produkter uppsatt samt lite enklare refaktorisering

--- src/api/routes/products/product.routes.js
+++ src/api/routes/products/product.routes.js
@@ -1,1 +1,14 @@
-//product routes
+// user routes
+import express from 'express';
+import ProductController from './product.controller.js';
+
+export const productsRouter = express.Router();
+
+productsRouter.get('/', ProductController.getAllProducts);
+productsRouter.get('/slug', ProductController.getProductWithSlug);
+productsRouter.get('/:id', ProductController.getProductWithId);
+
+productsRouter.post('/', ProductController.addProduct);
+
+productsRouter.delete('/:id', ProductController.deleteProduct);
+productsRouter.put('/:id', ProductController.updateProduct);
