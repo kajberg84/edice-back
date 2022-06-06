32b15687c659b0c3c5bcc7a8a732ff9a1e503710
Tinctoria <quizzie_89@hotmail.com>
2022-06-06 12:47:51 +0200

    dotenv to dependencies

--- src/mongoose/mongoose.js
+++ src/mongoose/mongoose.js
@@ -1,28 +1,28 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 export const connectToDB = async () => {
   // Bind connection to events (to get notifications).
-  mongoose.connection.on('connected', () =>
-    console.log('Connected to Mongo DB atlas')
+  mongoose.connection.on("connected", () =>
+    console.log("Connected to Mongo DB atlas")
   );
 
-  mongoose.connection.on('error', (err) =>
+  mongoose.connection.on("error", (err) =>
     console.error(`Mongoose connection error has occurred: ${err}`)
   );
 
-  mongoose.connection.on('disconnected', () =>
-    console.log('Mongoose connection is disconnected.')
+  mongoose.connection.on("disconnected", () =>
+    console.log("Mongoose connection is disconnected.")
   );
 
   // If the Node process ends, close the Mongoose connection.
-  process.on('SIGINT', () => {
+  process.on("SIGINT", () => {
     mongoose.connection.close(() => {
       console.log(
-        'Mongoose connection is disconnected due to application termination.'
+        "Mongoose connection is disconnected due to application termination."
       );
       process.exit(0);
     });
