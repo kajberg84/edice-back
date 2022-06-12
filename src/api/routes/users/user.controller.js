d561014603092df4f7a007dbc2389b7c13fff330
Mattis Erkensten <m.erkensten@icloud.com>
2022-06-12 21:54:09 +0200

    Refaktoriserat users controller med try catch och next()

--- src/api/routes/users/user.controller.js
+++ src/api/routes/users/user.controller.js
@@ -1,35 +1,49 @@
 // imports
-import { hashPassword } from "../../middleware/authentication.js";
-import User from "../../models/User.model.js";
-import StatusCodes from "../../helpers/StatusCodes.js";
+import { hashPassword } from '../../middleware/authentication.js';
+import User from '../../models/User.model.js';
+import StatusCodes from '../../helpers/StatusCodes.js';
 
 // helpers
-import { ErrorMessageHelper } from "../../helpers/ErrorMessageHelper.js";
+import { ErrorMessageHelper } from '../../helpers/ErrorMessageHelper.js';
 
 // Getting users
-const getAll = async (req, res) => {
-  const users = await User.find({});
-  res.status(StatusCodes.OK).json(users);
-  if (!users) {
+const getAll = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    res.status(StatusCodes.OK).json(users);
+    if (!users) {
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json('An error occured while getting user');
+    }
+  } catch (error) {
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json("An error occured while getting user");
+      .json(ErrorMessageHelper(error));
+    next();
   }
 };
 
 // Getting user by id
 const getUser = async (req, res) => {
-  const users = await User.findById(req.params.id);
-  res.status(StatusCodes.OK).json(users);
-  if (!users) {
+  try {
+    const users = await User.findById(req.params.id);
+    res.status(StatusCodes.OK).json(users);
+    if (!users) {
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json('An error occured while getting orders');
+    }
+  } catch (error) {
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json("An error occured while getting orders");
+      .json(ErrorMessageHelper(error));
+    next();
   }
 };
 
 // Adding a user
-const addUser = async (req, res) => {
+const addUser = async (req, res, next) => {
   const { name, address, city, zipcode, phone, email, password } = req.body;
   const newUser = new User({
     name,
@@ -42,14 +56,15 @@ const addUser = async (req, res) => {
   });
   try {
     await newUser.save();
-    res.status(StatusCodes.CREATED).json("User was created");
+    res.status(StatusCodes.CREATED).json('User was created');
   } catch (error) {
     res.status(StatusCodes.BAD_REQUEST).json(ErrorMessageHelper(error));
+    next();
   }
 };
 
 // update a user
-const updateUser = async (req, res) => {
+const updateUser = async (req, res, next) => {
   const { name, address, city, zipcode, phone, email, password } =
     await req.body;
   console.log(req.body);
@@ -67,23 +82,31 @@ const updateUser = async (req, res) => {
       },
       { new: true }
     );
-    res.status(StatusCodes.CREATED).json("User was updated successfully");
+    res.status(StatusCodes.CREATED).json('User was updated successfully');
   } catch (error) {
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json(ErrorMessageHelper(error));
+    next();
   }
 };
 
 // Delete a user
 const deleteUser = async (req, res) => {
-  User.findByIdAndDelete(req.params.id, (err) => {
-    if (err)
-      res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json("User could not be deleted due to server error");
-    res.status(StatusCodes.CREATED).json("User was deleted successfully");
-  });
+  try {
+    User.findByIdAndDelete(req.params.id, (err) => {
+      if (err)
+        res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json('User could not be deleted due to server error');
+      res.status(StatusCodes.CREATED).json('User was deleted successfully');
+    });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(ErrorMessageHelper(error));
+    next();
+  }
 };
 
 export { getAll, getUser, addUser, updateUser, deleteUser };
